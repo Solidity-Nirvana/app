af6d0e59063fba095d8af9e3b8ab38aefefd18ed
pseudonaut <jquarnstrom@yahoo.com>
2024-01-18 17:48:27 -0500

    Additions 2 / 3

--- components/home/cardEpisode.tsx
+++ components/home/cardEpisode.tsx
@@ -11,6 +11,8 @@ export default function CardEpisode({
   description,
   image,
   large,
+  premium,
+  membership
 }: {
   title: string;
   difficulty: string;
@@ -18,6 +20,8 @@ export default function CardEpisode({
   description: string;
   image: string;
   large?: boolean;
+  premium: boolean;
+  membership: string;
 }) {
   return (
     /// transition duration-500 hover:rotate-6
@@ -49,7 +53,7 @@ export default function CardEpisode({
           </Balancer>
         </h2>
         <div className="leading-normal text-gray-700 text-sm">
-          {description}
+          {description} {membership}
         </div>
       </div>
     </div>
