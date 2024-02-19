9a70d1cbb730d2b9e0a728ca3a1cce35501c133c
pseudonaut <jquarnstrom@yahoo.com>
2024-02-19 10:21:01 -0700

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { sfPro, inter } from "./fonts";
 import { Suspense } from "react";
-import Script from 'next/script';
+import { Metadata } from "next";
 
 import Nav from "@/components/layout/nav";
 import Footer from "@/components/layout/footer";
@@ -14,16 +14,36 @@ interface Window {
   gtag: (...args: any[]) => void;
 }
 
-export const metadata = {
-  title: "SolidityNirvana",
+export const metadata: Metadata = {
+  title: "SolidityNirvana | Ultimate Guide to Solidity",
   description:
-    "The ultimate guide to Solidity",
+    "Learn solidity with comprehensive guides and videos, this is a hands-on programming course with foundry-rs",
+  keywords:
+    [
+      "Solidity", "SolidityNirvana", "Solidity guide", "Solidity tutorial", "Solidity examples", "Learn Solidity",
+      "Solidity course", "Solidity foundry", "best solidity course 2024", "learn solidity"
+  ],
   twitter: {
     card: "summary_large_image",
     title: "SolidityNirvana",
     description:
       "The ultimate guide to Solidity",
     creator: "@SolidityNirvana",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://soliditynirvana.com",
+    siteName: "SolidityNirvana",
+    images: [
+      {
+        url: "https://www.soliditynirvana.com/_next/image?url=%2Fwaterfall.png&w=640&q=75",
+        width: 640,
+        height: 640,
+        alt: "SolidityNirvana",
+      },
+    ],
+    title: "SolidityNirvana | Ultimate Guide to Solidity",
+    description: "Learn solidity today with the ultimate guide, 2024",
   },
   metadataBase: new URL("https://soliditynirvana.com"),
   themeColor: "#000",
